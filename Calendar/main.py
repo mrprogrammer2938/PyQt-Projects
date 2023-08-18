72c11a8427780c1ddfee2da80c7faec6bede9992
Sina Meysami <78996423+mrprogrammer2938@users.noreply.github.com>
2023-08-18 18:41:48 +0330

    Update main.py

--- Calendar/main.py
+++ Calendar/main.py
@@ -1,42 +1,30 @@
 #!/usr/bin/python3
-# Calendar v1.0
 #
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from PyQt5.uic import loadUi
 import sys
 
-class Window(QWidget):
+class Window(QMainWindow):
     def __init__(self):
         super(Window,self).__init__()
-        global date
-        self.setWindowTitle("Calendar")
-        self.setGeometry(500,100,500,400)
-        self.setFixedSize(500,400)
-        cal = QCalendarWidget(self)
-        cal.setGridVisible(True)
-        cal.move(50,30)
-        cal.clicked[QDate].connect(self.show_date)
-        self.lbl = QLabel(self)
-        self.lbl.setFont(QFont("Arial",18))
-        self.lbl.setGeometry(145,255,400,100)
-        # self.lbl.move(170,300)
-        date = cal.selectedDate()
-        self.lbl.setText(date.toString("yyyy:MM:dd"))
-    
-    def show_date(self,date):
-        self.lbl.setText(date.toString())
+        loadUi("form.ui",self)
+        self.setWindowTitle("تقویم")
+        self.calendar.selectionChanged.connect(self.change_date)
+        
+    def change_date(self):
+        date = self.calendar.selectedDate()
+        self.label.setText(f"تاریخ:  {str(date.toPyDate())}")
         
         
 def main():
-    # Calendar v1.0
     app = QApplication(sys.argv)
-    app.setApplicationName("Calendar")
-    app.setApplicationVersion("v1.0")
     window = Window()
     window.show()
-    app.exec_()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
+    
